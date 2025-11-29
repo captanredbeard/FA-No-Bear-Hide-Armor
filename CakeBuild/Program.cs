3b600404a8c301042761bf21c66ee1b379229d09
captanredbeard <76228828+captanredbeard@users.noreply.github.com>
2025-11-29 10:54:48 -0600

    change zip name and sidedness

--- CakeBuild/Program.cs
+++ CakeBuild/Program.cs
@@ -108,7 +108,7 @@ namespace CakeBuild
             {
                 context.CopyFile($"../{BuildContext.ProjectName}/modicon.png", $"../Releases/{context.Name}/modicon.png");
             }
-            context.Zip($"../Releases/{context.Name}", $"../Releases/{context.Name}_{context.Version}.zip");
+            context.Zip($"../Releases/{context.Name}", $"../Releases/Forgotten Armory_Bear_Hide_Armor_Removal_Recipes_Only_{context.Version}.zip");
         }
     }
 
